683fade385ead54681dc2c9578fd1a25b4a76042
stevemasteller <stevermasteller@gmail.com>
2016-09-25 15:41:39 -0400

    Added board hover condition

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 "use strict";
 
 /*****************************************
-* 
+* Hover over game board
+******************************************/
+
+$(document).on("mouseover",".box", function() {
+	if ( !( $(this).hasClass("box-filled-1") || $(this).hasClass("box-filled-2")) ) {
+		if (gameState.isPlayer1.bool) {
+			$(this).css('background-image', 'url(img/o.svg)');
+		} else {
+			$(this).css('background-image', 'url(img/x.svg)');
+		}
+	}
+});
+
+$(document).on("mouseout",".box", function() {
+	if ( !( $(this).hasClass("box-filled-1") || $(this).hasClass("box-filled-2")) ) {
+		$(this).css('background-image', 'none');
+	}
+});
+
+
+/*****************************************
+* Display Start/Win/Tie
 ******************************************/
 var addWinScreenMarkup = function() {
     var endScreen  = '<div class="screen screen-win" id="finish">'
@@ -61,6 +82,7 @@ var displayWin = function(isWin) {
 var resetGame = function() {
 	$(document).find('li.box').removeClass(isO.box);
 	$(document).find('li.box').removeClass(isX.box);
+	$(document).find('li.box').css('background-image', 'none');
 	gameState.isPlayer1 = isO;
 	gameState.gameTurn = 0;
 }
@@ -98,13 +120,13 @@ var gameState = {
 		} else if (++this.gameTurn === 9) {
 			displayWin(false);
 		} 
-			
+		
 		if (this.isPlayer1.bool) {
 			this.isPlayer1 = isX;
 		} else {
 			this.isPlayer1 = isO;
 		}
-		gameState.highlightPlayer();
+		this.highlightPlayer();
 	},
 	
 	highlightPlayer: function() {
@@ -153,14 +175,16 @@ const VICTORY_CONDITIONS = [0x007, 	// 0 0000 0111
 							0x111,  // 1 0001 0001
 							0x054]; // 0 0101 0100
 var checkVictory = function() {
+	var andVictoryCondition;
 	
     for (var i = 0; i < VICTORY_CONDITIONS.length; i++) {
 		andVictoryCondition = VICTORY_CONDITIONS[i] & getCurrentBoard();
 
-		if ( andVictoryCondition == 0) {
+		if ( andVictoryCondition === 0) {
 			return true;
 		}
 	}
+	return false;
 }
 	
 // Event handler for clicking on the board.
